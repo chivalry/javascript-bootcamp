f3734038b7e59dc5d87c62eae203273457e2e760
Charles Ross <chivalry@mac.com>
2020-05-04 16:07:10 -0700

    Update todo.js

--- arrays/todo.js
+++ arrays/todo.js
@@ -2,6 +2,6 @@ const todos = ['task1', 'task2', 'task3', 'task4', 'task5']
 
 todos.splice(2, 1)
 todos.push('todo6')
-todos.unshift()
+todos.shift()
 console.log(`you have ${todos.length} todos`)
 console.log(todos)
