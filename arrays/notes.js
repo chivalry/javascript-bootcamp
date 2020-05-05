be7e270954c780ae75d5d269e4a25ed20b173a3c
Charles Ross <chivalry@mac.com>
2020-05-05 13:18:43 -0700

    Arrays 5

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise'
+}, {
+    title: 'Office modifications',
+    body: 'Get new chair'
+}]
 
 // console.log(notes.pop())
 // notes.push('My new note')
@@ -8,20 +17,18 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 // notes.splice(1, 1, 'This is the new second item')
 
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
-notes.forEach(function(item, index) {
-    console.log(index)
-    console.log(item)
-})
+// notes.forEach(function(item, index) {
+//     console.log(index)
+//     console.log(item)
+// })
 
 console.log(notes.length)
 console.log(notes)
 
-for (let count = 0; count < 3; count++) {
-    console.log(count)
-}
-
-for (let count = 0; count < notes.length; count++) {
-    console.log(notes[count])
-}
+const index = notes.findIndex(function(note, index) {
+    console.log(note)
+    return note.title === 'Habbits to work on'
+})
+console.log(index)
--- arrays/todo.js
+++ arrays/todo.js
@@ -7,6 +7,3 @@ console.log(`you have ${todos.length} todos`)
 todos.forEach(function(item, index) {
     console.log(`${index + 1}. ${item}`)
 })
-for (let i = 0; i < todos.length; i++) {
-    console.log(`${i + 1}. ${todos[i]}`)
-}
