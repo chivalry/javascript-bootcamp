8cbf941ee14a25856e06376bf3098973e4f59283
Charles Ross <chivalry@mac.com>
2020-05-06 17:24:59 -0700

    browser exercise 11

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,6 +9,24 @@ const notes = [{
     body: 'Get new chair'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    document.querySelector('#notes').innerHTML = ''
+    filteredNotes.forEach(function(note) {
+        const noteElement = document.createElement('p')
+        noteElement.textContent = note.title
+        document.querySelector('#notes').appendChild(noteElement)
+    })
+}
+
+renderNotes(notes, filters)
+
 document.querySelector('#create-note').addEventListener('click', function(event) {
     event.target.textContent = 'The button was clicked'
 })
@@ -20,5 +38,6 @@ document.querySelector('#remove-all').addEventListener('click', function(event) 
 })
 
 document.querySelector('#search-text').addEventListener('input', function(event) {
-    console.log(event.target.value)
+    filters.searchText = event.target.value
+    renderNotes(notes, filters)
 })
