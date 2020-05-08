e1cf12b5ddb3cbc32923a3422c147897df57e077
Charles Ross <chivalry@mac.com>
2020-05-08 14:59:04 -0700

    Chapter 10 section 5

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,8 +1,14 @@
 const Hangman = function (word, count) {
-    this.word = word
+    this.word = word.toLowerCase().split('')
     this.count = count
+    this.letters = []
+}
+Hangman.prototype.getPuzzle = function () {
+    let buf = ''
+    this.word.forEach((letter) => buf += letter === ' ' ? ' ' : this.letters.includes(letter) ? letter : '*')
+    return buf
 }
 let game = new Hangman('cat', 5)
-console.log(game)
-game = new Hangman('blah', 8)
-console.log(game)
+console.log(game.getPuzzle())
+game = new Hangman('blah blah', 8)
+console.log(game.getPuzzle())
