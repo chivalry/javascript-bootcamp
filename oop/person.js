eeaed0668b724cffd173efe0f4fe746187b0a7e0
Charles Ross <chivalry@mac.com>
2020-05-08 14:43:18 -0700

    Chapter 10 section 4

--- oop/person.js
+++ oop/person.js
@@ -1,9 +1,23 @@
-const Person = function (firstName, lastName, age) {
+const Person = function (firstName, lastName, age, likes) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
+    this.likes = likes
 }
-const me = new Person('Chuck', 'Ross', 48)
-console.log(me)
-const person = new Person('Mellie', 'Ross', 57)
-console.log(person)
+Person.prototype.getBio = function() {
+    let bio = `${this.firstName} is ${this.age}.`
+    this.likes.forEach((like) => {
+        bio += ` ${this.firstName} likes ${like}.`
+    })
+    return bio
+}
+Person.prototype.setName = function(fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+const me = new Person('Chuck', 'Ross', 48, ['chess', 'star trek'])
+me.setName('Charles Ross')
+console.log(me.getBio())
+const person = new Person('Mellie', 'Ross', 57, [])
+console.log(person.getBio())
