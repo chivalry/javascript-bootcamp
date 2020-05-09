2e07269c82232a1e8c9767f8d962122b41c849fd
Charles Ross <chivalry@mac.com>
2020-05-09 14:41:28 -0700

    Chapter 11 section 7

--- hangman/app.js
+++ hangman/app.js
@@ -10,24 +10,18 @@ window.addEventListener('keypress', function (event) {
 })
 
 
-getPuzzle((error, puzzle) => {
+getPuzzle(2, (error, puzzle) => {
     if (error) {
         console.log(`Error: ${error}`)
     } else {
         console.log(puzzle)
     }
 })
-console.log('do something else')
-const puzz = getPuzzleSync()
-console.log(puzz)
 
-// const usReq = new XMLHttpRequest()
-// usReq.addEventListener('readystatechange', (event) => {
-//     if (event.target.readyState === 4 && event.target.status === 200) {
-//         const countries = JSON.parse(event.target.responseText)
-//         const country = countries.find((country) => country.alpha2Code === 'US')
-//         console.log(country.name)
-//     }
-// })
-// usReq.open('GET', 'http://restcountries.eu/rest/v2/all')
-// usReq.send()
+const country = getCountry('MX', (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(country.name)
+    }
+})
--- hangman/requests.js
+++ hangman/requests.js
@@ -1,4 +1,4 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount, callback) => {
     const request = new XMLHttpRequest()
     request.addEventListener('readystatechange', (event) => {
         if (event.target.readyState === 4 && event.target.status === 200) {
@@ -8,18 +8,21 @@ const getPuzzle = (callback) => {
             callback('An error has taken place', undefined)
         }
     })
-    request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+    request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     request.send()
 }
 
-const getPuzzleSync = () => {
+const getCountry = (code, callback) => {
     const request = new XMLHttpRequest()
-    request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3', false)
+    request.addEventListener('readystatechange', (event) => {
+        if (event.target.readyState === 4 && event.target.status === 200) {
+            const countries = JSON.parse(event.target.responseText)
+            const country = countries.find((country) => country.alpha2Code === code)
+            callback(undefined, country)
+        } else if (event.target.readyState === 4) {
+            callback('An error has occurred', undefined)
+        }
+    })
+    request.open('GET', 'http://restcountries.eu/rest/v2/all')
     request.send()
-    if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText)
-        return data.puzzle
-    } else if (request.readyState === 4) {
-        throw new Error('things went badly')
-    }
 }
