68eceb364c7bd0a0d6d5b14a720849c9e8729a85
Charles Ross <chivalry@mac.com>
2020-05-08 18:10:27 -0700

    Chapter 10 section 11

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -3,7 +3,7 @@ const Hangman = function (word, count) {
     this.count = count
     this.letters = []
     this.status = 'playing'
-    this.getStatus()
+    this.updateStatus()
 }
 
 Hangman.prototype.getPuzzle = function () {
@@ -13,6 +13,9 @@ Hangman.prototype.getPuzzle = function () {
 }
 
 Hangman.prototype.guess = function (letter) {
+    if (this.status !== 'playing') {
+        return
+    }
     letter = letter.toLowerCase()
     if (this.letters.includes(letter)) {
         return
@@ -21,15 +24,24 @@ Hangman.prototype.guess = function (letter) {
     if (!this.word.includes(letter)) {
         this.count--
     }
-    this.getStatus()
+    this.updateStatus()
 }
 
-Hangman.prototype.getStatus = function () {
+Hangman.prototype.updateStatus = function () {
     if (this.count === 0) {
         this.status = 'failed'
     } else {
         const finished = this.word.every((letter) => this.letters.includes(letter))
         this.status = finished ? 'finished' : this.status
     }
-    console.log(this.status)
+}
+
+Hangman.prototype.statusMessage = function () {
+    if (this.status == 'playing') {
+        return `Guesses left: ${this.count}`
+    } else if (this.status == 'failed') {
+        return `Nice try! The word was "${this.word.join('')}".`
+    } else {
+        return 'Great work! You guessed the word.'
+    }
 }
