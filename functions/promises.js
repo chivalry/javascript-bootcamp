8d6ed750c037845d6947f9f4cad46430e8d713b2
Charles Ross <chivalry@mac.com>
2020-05-09 15:33:42 -0700

    Chapter 11, section 9

---
+++ functions/promises.js
@@ -0,0 +1,31 @@
+const getDataCallback = (callback) => {
+    setTimeout(() => {
+        callback('This is my callback error', undefined)
+    }, 2000)
+}
+getDataCallback((err, data) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+})
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve('This is the promise data')
+        reject('This is my promise error')
+        reject('This is my duplicate promise error')
+    }, 2000)
+})
+myPromise.then((data) => {
+    console.log(data)
+}, (err) => {
+    console.log(err)
+})
+
+myPromise.then((data) => {
+    console.log(data)
+}, (err) => {
+    console.log(err)
+})
