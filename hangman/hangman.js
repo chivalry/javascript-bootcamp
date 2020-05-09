ebc5e21da3134990da69724451daaa56947ab538
Charles Ross <chivalry@mac.com>
2020-05-08 18:23:05 -0700

    Chapter 10 section 12

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,47 +1,49 @@
-const Hangman = function (word, count) {
-    this.word = word.toLowerCase().split('')
-    this.count = count
-    this.letters = []
-    this.status = 'playing'
-    this.updateStatus()
-}
-
-Hangman.prototype.getPuzzle = function () {
-    let buf = ''
-    this.word.forEach((letter) => buf += letter === ' ' ? ' ' : this.letters.includes(letter) ? letter : '*')
-    return buf
-}
-
-Hangman.prototype.guess = function (letter) {
-    if (this.status !== 'playing') {
-        return
+class Hangman {
+    constructor(word, count) {
+        this.word = word.toLowerCase().split('')
+        this.count = count
+        this.letters = []
+        this.status = 'playing'
+        this.updateStatus()
     }
-    letter = letter.toLowerCase()
-    if (this.letters.includes(letter)) {
-        return
-    }
-    this.letters.push(letter)
-    if (!this.word.includes(letter)) {
-        this.count--
-    }
-    this.updateStatus()
-}
 
-Hangman.prototype.updateStatus = function () {
-    if (this.count === 0) {
-        this.status = 'failed'
-    } else {
-        const finished = this.word.every((letter) => this.letters.includes(letter))
-        this.status = finished ? 'finished' : this.status
+    getPuzzle() {
+        let buf = ''
+        this.word.forEach((letter) => buf += letter === ' ' ? ' ' : this.letters.includes(letter) ? letter : '*')
+        return buf
     }
-}
 
-Hangman.prototype.statusMessage = function () {
-    if (this.status == 'playing') {
-        return `Guesses left: ${this.count}`
-    } else if (this.status == 'failed') {
-        return `Nice try! The word was "${this.word.join('')}".`
-    } else {
-        return 'Great work! You guessed the word.'
+    guess(letter) {
+        if (this.status !== 'playing') {
+            return
+        }
+        letter = letter.toLowerCase()
+        if (this.letters.includes(letter)) {
+            return
+        }
+        this.letters.push(letter)
+        if (!this.word.includes(letter)) {
+            this.count--
+        }
+        this.updateStatus()
+    }
+
+    updateStatus() {
+        if (this.count === 0) {
+            this.status = 'failed'
+        } else {
+            const finished = this.word.every((letter) => this.letters.includes(letter))
+            this.status = finished ? 'finished' : this.status
+        }
+    }
+
+    statusMessage() {
+        if (this.status == 'playing') {
+            return `Guesses left: ${this.count}`
+        } else if (this.status == 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`
+        } else {
+            return 'Great work! You guessed the word.'
+        }
     }
 }
--- oop/person.js
+++ oop/person.js
@@ -1,20 +1,24 @@
-const Person = function (firstName, lastName, age, likes) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
-Person.prototype.getBio = function() {
-    let bio = `${this.firstName} is ${this.age}.`
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-    return bio
-}
-Person.prototype.setName = function(fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
+class Person {
+    constructor(firstName, lastName, age, likes=[]) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+        return bio
+    }
+
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 const me = new Person('Chuck', 'Ross', 48, ['chess', 'star trek'])
 me.setName('Charles Ross')
