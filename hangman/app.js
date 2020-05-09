4fe4e4e9cc63688b2019d6b0bfb04cd448f0d7d7
Charles Ross <chivalry@mac.com>
2020-05-09 13:25:00 -0700

    Chapter 11 section 3

--- hangman/app.js
+++ hangman/app.js
@@ -11,10 +11,12 @@ window.addEventListener('keypress', function (event) {
 
 const request = new XMLHttpRequest()
 request.addEventListener('readystatechange', (event) => {
-    if (event.target.readyState === 4) {
+    if (event.target.readyState === 4 && event.target.status === 200) {
         const data = JSON.parse(event.target.responseText)
         console.log(data)
+    } else if (event.target.readyState === 4) {
+        console.log('An error has taken place')
     }
 })
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
