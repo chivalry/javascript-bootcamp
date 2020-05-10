e007c035f4cdb930441ce441eaf5f2f5e191c8a0
Charles Ross <chivalry@mac.com>
2020-05-09 19:54:04 -0700

    Chapter 11 section 13

--- hangman/app.js
+++ hangman/app.js
@@ -16,11 +16,11 @@ getPuzzle(2).then((puzzle) => {
     console.log(`Error: ${err}`)
 })
 
-// getCountry('MX').then((country) => {
-//     console.log(country.name)
-// }, (err) => {
-//     console.log(err)
-// })
+getCountry('US').then((country) => {
+    console.log(country.name)
+}).catch((err) => {
+    console.log(`Error: ${err}`)
+})
 
 // fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
 //     if (response.status === 200) {
--- hangman/requests.js
+++ hangman/requests.js
@@ -5,22 +5,15 @@ const getPuzzle = (wordCount) => {
         } else {
             throw new Error('Unable to fetch puzzle')
         }
-    }).then((data) => {
-        return data.puzzle
-    })
+    }).then((data) => data.puzzle)
 }
 
-const getCountry = (code) => new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest()
-    request.addEventListener('readystatechange', (event) => {
-        if (event.target.readyState === 4 && event.target.status === 200) {
-            const countries = JSON.parse(event.target.responseText)
-            const country = countries.find((country) => country.alpha2Code === code)
-            resolve(country)
-        } else if (event.target.readyState === 4) {
-            reject('An error has occurred')
+const getCountry = (code) => {
+    return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
+        if (response.status === 200) {
+            return response.json()
+        } else {
+            throw new Error('Unable to fetch countries')
         }
-    })
-    request.open('GET', 'http://restcountries.eu/rest/v2/all')
-    request.send()
-})
+    }).then((countries) => countries.find((country) => country.alpha2Code === code))
+}
